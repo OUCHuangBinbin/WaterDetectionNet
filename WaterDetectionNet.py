2f5d3dbd32c11cc77d54a30c2a570347970bd865
OUCHuangBinbin <115345313+OUCHuangBinbin@users.noreply.github.com>
2024-05-13 15:07:58 +0800

    Update WaterDetectionNet.py

--- WaterDetectionNet.py
+++ WaterDetectionNet.py
@@ -2,4 +2,5 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 ##Real-time online updates
+#This paper is currently in the review stage, the code is not convenient to fully disclose, will be open source after seeing the publication.
 #If you want more details, please contact me.My e-mail huangbinbin@stu.ouc.edu.cn; 
